e3190f27b171e8a217995da417a813b3f08fbd04
Matheus Alves <theuves@outlook.com>
2015-09-11 14:09:41 -0400

    Unused 'fs'.

--- cli.js
+++ cli.js
@@ -2,7 +2,6 @@
 
 var portunhol = require('./portunhol.js').portunholate;
 var lazy = require("lazy");
-var fs  = require("fs");
 
 new lazy(process.stdin).lines.forEach(function(line) {
   console.log(portunhol(line.toString()));
